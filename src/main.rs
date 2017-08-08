7a0c933245d266a2a40df5df8b0a937278c049bd
Andrea Jemmett <andreajemmett@gmail.com>
2017-08-08 14:48:41 +0200

    some refactoring

--- src/main.rs
+++ src/main.rs
@@ -30,61 +30,69 @@ impl fmt::Display for FuzzMonitor {
     }
 }
 
-fn parse_args<'a>() -> Result<FuzzMonitor, &'a str> {
-    let mut options = FuzzMonitor { tool: MonitoringTool::Perf, sut: vec![] };
-    let mut do_sut = false;
-    for arg in args() {
-        match arg.as_str() {
-            x if do_sut == true => options.sut.push(x.to_string()),
-            "-p" => options.tool = MonitoringTool::Perf,
-            "-q" => options.tool = MonitoringTool::Qemu,
-            "--" => do_sut = true,
-            _ => ()
+impl FuzzMonitor {
+    fn default() -> FuzzMonitor {
+        FuzzMonitor { tool: MonitoringTool::Perf, sut: vec![] }
+    }
+
+    fn from_args<'a>() -> Result<FuzzMonitor, &'a str> {
+        let mut options = FuzzMonitor::default();
+        let mut do_sut = false;
+        for arg in args() {
+            match arg.as_str() {
+                x if do_sut == true => options.sut.push(x.to_string()),
+                "-p" => options.tool = MonitoringTool::Perf,
+                "-q" => options.tool = MonitoringTool::Qemu,
+                "--" => do_sut = true,
+                _ => ()
+            }
+        }
+        if options.sut.is_empty() {
+            Err("usage: fuzz-monitor [-p|-q] -- command [args]")
+        } else {
+            Ok(options)
         }
     }
-    if options.sut.is_empty() {
-        Err("usage: fuzz-monitor [-p|-q] -- command [args]")
-    } else {
-        Ok(options)
+
+    fn start(self) {
+        assert!(!self.sut.is_empty());
+        println!("[+] starting fuzz-monitor ({})", self);
+
+        let context = zmq::Context::new();
+        let receiver = context.socket(zmq::PULL).expect("failed to create zmq context");
+        receiver.bind("tcp://*:5558").expect("failed to bind to tcp://*:5558");
+        println!("[+] listening...");
+
+        let mut max_count = 0;
+        let sut_s = self.sut.iter().map(|s| s.as_str()).collect::<Vec<&str>>();
+        let sut = &sut_s.as_slice();
+
+        loop {
+            let bytes = receiver.recv_bytes(0).unwrap();
+
+            let now = Instant::now();
+            let count = match self.tool {
+                MonitoringTool::Qemu => qemu::trace(bytes, sut).len() as u64,
+                MonitoringTool::Perf => perf::trace(bytes, sut)
+            };
+            let elapsed = now.elapsed();
+
+            let mut new_max = false;
+            if count > max_count {
+                max_count = count;
+                new_max = true;
+            }
+
+            let ms = elapsed.as_secs() * 1000 + (elapsed.subsec_nanos() as u64 / 1000000);
+            println!("[{}] {} (max {}, in: {}ms)", if new_max {'!'} else {'?'}, count, max_count, ms);
+        }
     }
 }
 
-fn start_monitoring(options: FuzzMonitor) {
-    println!("[+] starting fuzz-monitor ({})", options);
-
-    let context = zmq::Context::new();
-    let receiver = context.socket(zmq::PULL).expect("failed to create zmq context");
-    receiver.bind("tcp://*:5558").expect("failed to bind to tcp://*:5558");
-    println!("[+] listening...");
-
-    let mut max_count = 0;
-    let sut_s = options.sut.iter().map(|s| s.as_str()).collect::<Vec<&str>>();
-    let sut = &sut_s.as_slice();
-
-    loop {
-        let bytes = receiver.recv_bytes(0).unwrap();
-
-        let now = Instant::now();
-        let count = match options.tool {
-            MonitoringTool::Qemu => qemu::trace(bytes, sut).len() as u64,
-            MonitoringTool::Perf => perf::trace(bytes, sut)
-        };
-        let elapsed = now.elapsed();
-
-        let mut new_max = false;
-        if count > max_count {
-            max_count = count;
-            new_max = true;
-        }
-
-        let ms = elapsed.as_secs() * 1000 + (elapsed.subsec_nanos() as u64 / 1000000);
-        println!("[{}] {} (max {}, in: {}ms)", if new_max {'!'} else {'?'}, count, max_count, ms);
-    }
-}
 
 fn main() {
-    match parse_args() {
-        Ok(opt) => start_monitoring(opt),
+    match FuzzMonitor::from_args() {
+        Ok(fuzz_monitor) => fuzz_monitor.start(),
         Err(e) => println!("{}", e)
     }
 }
